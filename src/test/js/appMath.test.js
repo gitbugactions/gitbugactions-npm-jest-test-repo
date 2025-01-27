bad1d309fdcc0ebaf367690599480c0a38df54db
André Silva <andre15andre@hotmail.com>
2025-01-27 18:23:07 +0100

    initial commit

---
+++ src/test/js/appMath.test.js
@@ -0,0 +1,29 @@
+const AppMath = require('../../main/js/appMath');
+
+describe('AppMath', () => {
+    let appMath;
+
+    beforeEach(() => {
+        appMath = new AppMath();
+    });
+
+    test('sum adds two numbers correctly', () => {
+        // expect(appMath.sum(1, 2)).toBe(3);
+    });
+
+    test('subtract subtracts two numbers correctly', () => {
+        expect(appMath.subtract(2, 1)).toBe(1);
+    });
+
+    test('multiply multiplies two numbers correctly', () => {
+        expect(appMath.multiply(2, 2)).toBe(4);
+    });
+
+    test('sumTwice adds the second number twice', () => {
+        expect(appMath.sumTwice(2, 2)).toBe(6);
+    });
+
+    test('subtractTwice subtracts the second number twice', () => {
+        expect(appMath.subtractTwice(6, 2)).toBe(2);
+    });
+});
